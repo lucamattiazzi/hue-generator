58cfc2aea393ac679eb8e77e24111a386cf6cb51
Yeasteregg <l.d.mattiazzi@gmail.com>
2018-09-04 17:12:18 +0200

    working starting angle

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const MAX = 360
 const PHI = (1 + Math.sqrt(5)) / 2
 
-function* hueGenerator(saturation = 50, lightness = 50, startingIdx = 0, maxSize = Infinity) {
-	this.idx = startingIdx % MAX
+function* hueGenerator(saturation = 50, lightness = 50, startingAngle = 0, maxSize = Infinity) {
+	this.idx = 0
 	const getString = hue => `hsl(${hue}, ${saturation}%, ${lightness}%)`
 	while (this.idx < maxSize) {
-		const hue = Math.round((this.idx * PHI * MAX) % MAX)
+		const angle = startingAngle + this.idx * PHI * MAX
+		const hue = Math.round(angle % MAX)
 		yield getString(hue)
 		this.idx++
 	}
 }
 
-function getGenerator(saturation = 50, lightness = 50, startingIdx = 0) {
+function getGenerator(saturation = 50, lightness = 50, startingAngle = 0) {
 	const self = {}
 	self.next = function() {
-		self.generator = self.generator || hueGenerator(saturation, lightness, startingIdx)
+		self.generator = self.generator || hueGenerator(saturation, lightness, startingAngle)
 		return self.generator.next().value
 	}
 	self.asArray = function(length = 1e4) {
-		const limitedGenerator = hueGenerator(saturation, lightness, startingIdx, length)
+		const limitedGenerator = hueGenerator(saturation, lightness, startingAngle, length)
 		const array = Array.from(limitedGenerator)
 		return array
 	}
