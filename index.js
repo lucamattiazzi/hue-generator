40cc812ff9fb829b241448de2b4a6898dbd5febb
Yeasteregg <l.d.mattiazzi@gmail.com>
2018-09-04 18:07:09 +0200

    added get idx

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const MAX = 360
 const PHI = (1 + Math.sqrt(5)) / 2
 
-function* hueGenerator(saturation = 50, lightness = 50, startingAngle = 0, maxSize = Infinity) {
-	this.idx = 0
-	const getString = hue => `hsl(${hue}, ${saturation}%, ${lightness}%)`
-	while (this.idx < maxSize) {
-		const angle = startingAngle + this.idx * PHI * MAX
-		const hue = Math.round(angle % MAX)
-		yield getString(hue)
-		this.idx++
-	}
-}
-
 function getGenerator(saturation = 50, lightness = 50, startingAngle = 0) {
-	const self = {}
+	const self = { idx: 0, saturation, lightness, startingAngle }
 	self.next = function() {
-		self.generator = self.generator || hueGenerator(saturation, lightness, startingAngle)
-		return self.generator.next().value
+		return self.getIdx(++self.idx)
+	}
+	self.getIdx = function(idx) {
+		const angle = self.startingAngle + idx * PHI * MAX
+		const hue = Math.round(angle % MAX)
+		return `hsl(${hue}, ${self.saturation}%, ${self.lightness}%)`
 	}
 	self.asArray = function(length = 1e4) {
-		const limitedGenerator = hueGenerator(saturation, lightness, startingAngle, length)
-		const array = Array.from(limitedGenerator)
+		const array = Array.from({ length }, (_, idx) => self.getIdx(idx))
 		return array
+	}
+	self.reset = function() {
+		self.idx = 0
 	}
 	return self
 }
